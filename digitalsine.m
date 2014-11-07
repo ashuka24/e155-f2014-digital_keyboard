d5c91bb374899b6e61d522c2563ff66853264215
SKrupa <rakuth@gmail.com>
2014-11-07 02:06:13 -0800

    Update digitalsine.m

--- digitalsine.m
+++ digitalsine.m
@@ -7,5 +7,5 @@ for i=0:63
      num = num*8;
      num2 = round(num);
      binary(i+1) = num2;
-     disp(['4''d',num2str(i), ' : wave = 4''b', num2str(dec2bin(num,4)), ';    // exact:', num2str(num/16), '       approx:', num2str(num2/16)]);
+     disp(['6''d',num2str(i), ' : wave = 4''b', num2str(dec2bin(num,4)), ';    // exact:', num2str(num/16), '       approx:', num2str(num2/16)]);
 end
