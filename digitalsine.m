4919e12d08843c9ab21749bf6b6eb0457dc8345a
SKrupa <rakuth@gmail.com>
2014-11-06 21:27:47 -0800

    Create digitalsine.m

---
+++ digitalsine.m
@@ -0,0 +1,11 @@
+decimal = zeros(64,1);
+binary = zeros(64,4);
+num = 0;
+for i=0:63
+     num = sin(2*pi*i/64) + 1;
+     decimal(i+1) = num;
+     num = num*8;
+     num2 = round(num);
+     binary(i+1) = num2;
+     disp(['4''d',num2str(i), ' : wave = 4''b', num2str(dec2bin(num,4)), ';    // exact:', num2str(num/16), '       approx:', num2str(num2/16)]);
+end
