31060964d6cd662f96122c3552e8de3ebd24640e
SKrupa <rakuth@gmail.com>
2014-11-16 20:56:36 -0800

    Update piano.sv

--- piano.sv
+++ piano.sv
@@ -10,9 +10,12 @@ module digital_keyboard(input  logic       sck, sdi, clk,
 
 	logic [31:0] q;
 	logic sdi,
+	logic [7:0] notes, note1, note2, note3;
     
 	spi_slave_receive_only spi(sck, sdi, q);
-	attenuation sound(clk, q, wave);
+	process_spi proc(sck, clk, q, note1, note2, note3);
+	add_notes add(note1, note2, note3, notes);
+	attenuation sound(clk, notes, wave);
     
 
 endmodule
@@ -24,6 +27,33 @@ module spi_slave_receive_only(input  logic       sck, //from master
                               output logic [31:0] q); // data received
 	always_ff @(posedge sck)
 		q <={q[30:0], sdi}; //shift register
+endmodule
+
+module process_spi( input logic         sck, clk,
+                    input logic  [31:0]  q,
+                    output logic [7:0] note1, note2, note3);
+    logic [6:0] cnt = 6'b00_0000;
+    logic moved = 1'b0; //makes sure iterator doesn't start until initial signal
+	 
+	 always_ff @(negedge sck)
+		if(cnt == 6'd31)
+		begin
+			note1 <= q[7:0];
+			note2 <= q[15:8];
+			note3 <= q[23:16];
+			cnt <= '0;
+		end
+		else
+			cnt <= cnt + 1'b1;
+			
+endmodule
+
+module add_notes(input logic [7:0] note1, note2, note3,
+		output logic [7:0] notes);
+	logic [9:0] intermed;
+	assign intermed = (note1 + note2 + note3);
+	assign notes = intermed>>2 + intermed>>4 + intermed>>6; //divide by 3.047 = ~3
+
 endmodule
 
 module attenuation(input  logic       clk,
