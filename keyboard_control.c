f5ec706627f2b4cc8ce7a75e204081a2771dcf3a
ashuka24 <axue@hmc.edu>
2014-11-16 21:30:20 -0800

    because git is being stupid.... keyboard stuff

--- keyboard_control.c
+++ keyboard_control.c
@@ -18,10 +18,11 @@ void initSquare(void);
 void initSawtooth(void);
 void initTriangle(void);
 void initSine(void);
+void initPeriods(void);
 
 void getPeriods(unsigned int);
-unsigned int octave_adjust();
-void octave_read();
+unsigned int octave_adjust(unsigned int);
+void octave_read(void);
 void getWave(unsigned int);
 
 /*****************************************************************************
@@ -31,13 +32,13 @@ unsigned char octave = 4; // default on middle C
 unsigned int notearray[12];
 unsigned int periods[3];
 unsigned int secondtonano = 10^9; //convert seconds to picoseconds
-unsigned int periodarray[12];
-unsigned char square[512], 
-            sawtooth[512], 
-            triangle[512], 
-                sine[512], 
-            currWave[512];
-unsigned char canupdate = 1;
+unsigned int *periodarray;
+unsigned char *square, 
+              *sawtooth, 
+              *triangle, 
+              *sine,
+              *currWave;
+unsigned char canUpdate = 1;
 
 /*****************************************************************************
  Main
@@ -54,13 +55,17 @@ void main(void) {
 	initSawtooth();
 	initTriangle();
 	initSine();
+	initPeriods();
+
     TMR2 = 0;
     TMR3 = 0;
     TMR4 = 0;
+
     unsigned short count1 = 0, count2 = 0, count3 = 0;
     unsigned short notes = 0;
     unsigned char send1, send2, send3;
     unsigned int sendtot;
+
 	while(1){
         octave_read();
         notes = PORTB>>4;
@@ -71,25 +76,25 @@ void main(void) {
         if(periods[0] == 0) {
             send1 = 0;
         } else if(TMR2*4 >= periods[0]/512) {
-            send1 = currwave[++count1];
+            send1 = currWave[++count1];
         } else {
-            send1 = currwave[count1];
+            send1 = currWave[count1];
         }
         
         if(periods[1] == 0) {
             send2 = 0;
         } else if(TMR3*4 >= periods[1]/512) {
-            send2 = currwave[++count1];
+            send2 = currWave[++count1];
         } else {
-            send2 = currwave[count1];
+            send2 = currWave[count1];
         }
         
         if(periods[2] == 0) {
             send3 = 0;
         } else if(TMR4*4 >= periods[2]/512) {
-            send3 = currwave[++count1];
+            send3 = currWave[++count1];
         } else {
-            send3 = currwave[count1];
+            send3 = currWave[count1];
         }
 
         sendtot = send1 + send2<<8 + send3<<16;    
@@ -98,7 +103,6 @@ void main(void) {
 	}
 	 
 }
-
 
 /******************************************************************************
  SPI Interfacing
@@ -143,76 +147,81 @@ void initTimers(void){
 }
 
 void initSquare(void){
-	for(int i = 0; i < 256; i++){
+	int i = 0;
+	for(; i < 256; i++){
 		square[i]=255;
 		square[512-i] = 0;
 	}
 }
 
 void initSawtooth(void){
-	for(int i = 0;i < 512; i++){
+	int i = 0;
+	for(;i < 512; i++){
 		sawtooth[i] = i/2;
 	}
 }
 
 void initTriangle(void){
-	for(int i = 0; i < 256; i++){
+	int i = 0;
+	for(; i < 256; i++){
 		square[i]=i;
 		square[511-i] = i;
 	}
 }
 
 void initSine(void){
-	sine = {128, 130, 131, 133, 134, 136, 137, 139, 141, 142, 144, 145, 147, 148, 150, 151, 153, 
-			155, 156, 158, 159, 161, 162, 164, 165, 167, 168, 170, 171, 173, 174, 176, 177, 178, 
-			180, 181, 183, 184, 186, 187, 188, 190, 191, 192, 194, 195, 196, 198, 199, 200, 202, 
-			203, 204, 206, 207, 208, 209, 210, 212, 213, 214, 215, 216, 217, 219, 220, 221, 222, 
-			223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 234, 235, 236, 237, 238, 
-			239, 239, 240, 241, 242, 242, 243, 244, 244, 245, 246, 246, 247, 247, 248, 249, 249, 
-			250, 250, 250, 251, 251, 252, 252, 253, 253, 253, 254, 254, 254, 254, 255, 255, 255, 
-			255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 
-			255, 255, 255, 255, 255, 254, 254, 254, 254, 253, 253, 253, 252, 252, 251, 251, 250, 
-			250, 250, 249, 249, 248, 247, 247, 246, 246, 245, 244, 244, 243, 242, 242, 241, 240, 
-			239, 239, 238, 237, 236, 235, 234, 234, 233, 232, 231, 230, 229, 228, 227, 226, 225, 
-			224, 223, 222, 221, 220, 219, 217, 216, 215, 214, 213, 212, 210, 209, 208, 207, 206, 
-			204, 203, 202, 200, 199, 198, 196, 195, 194, 192, 191, 190, 188, 187, 186, 184, 183, 
-			181, 180, 178, 177, 176, 174, 173, 171, 170, 168, 167, 165, 164, 162, 161, 159, 158, 
-			156, 155, 153, 151, 150, 148, 147, 145, 144, 142, 141, 139, 137, 136, 134, 133, 131, 
-			130, 128, 126, 125, 123, 122, 120, 119, 117, 115, 114, 112, 111, 109, 108, 106, 105, 
-			103, 101, 100, 98, 97, 95, 94, 92, 91, 89, 88, 86, 85, 83, 82, 80, 79, 78, 76, 75,
-			73, 72, 70, 69, 68, 66, 65, 64, 62, 61, 60, 58, 57, 56, 54, 53, 52, 50, 49, 48, 47, 
-		    46, 44, 43, 42, 41, 40, 39, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 
-		    23, 22, 22, 21, 20, 19, 18, 17, 17, 16, 15, 14, 14, 13, 12, 12, 11, 10, 10, 9, 9, 8, 
-		    7, 7, 6, 6, 6, 5, 5, 4, 4, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 
-		    0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 6, 7, 
-		    7, 8, 9, 9, 10, 10, 11, 12, 12, 13, 14, 14, 15, 16, 17, 17, 18, 19, 20, 21, 22, 22, 
-		    23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 40, 41, 42, 43, 44, 
-		    46, 47, 48, 49, 50, 52, 53, 54, 56, 57, 58, 60, 61, 62, 64, 65, 66, 68, 69, 70, 72, 
-		    73, 75, 76, 78, 79, 80, 82, 83, 85, 86, 88, 89, 91, 92, 94, 95, 97, 98, 100, 101, 
-		    103, 105, 106, 108, 109, 111, 112, 114, 115, 117, 119, 120, 122, 123, 125, 126};
+	sine = (char[]) {128, 130, 131, 133, 134, 136, 137, 139, 141, 142, 144, 145, 147, 148, 150, 151, 153, 
+					155, 156, 158, 159, 161, 162, 164, 165, 167, 168, 170, 171, 173, 174, 176, 177, 178, 
+					180, 181, 183, 184, 186, 187, 188, 190, 191, 192, 194, 195, 196, 198, 199, 200, 202, 
+					203, 204, 206, 207, 208, 209, 210, 212, 213, 214, 215, 216, 217, 219, 220, 221, 222, 
+					223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 234, 235, 236, 237, 238, 
+					239, 239, 240, 241, 242, 242, 243, 244, 244, 245, 246, 246, 247, 247, 248, 249, 249, 
+					250, 250, 250, 251, 251, 252, 252, 253, 253, 253, 254, 254, 254, 254, 255, 255, 255, 
+					255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 
+					255, 255, 255, 255, 255, 254, 254, 254, 254, 253, 253, 253, 252, 252, 251, 251, 250, 
+					250, 250, 249, 249, 248, 247, 247, 246, 246, 245, 244, 244, 243, 242, 242, 241, 240, 
+					239, 239, 238, 237, 236, 235, 234, 234, 233, 232, 231, 230, 229, 228, 227, 226, 225, 
+					224, 223, 222, 221, 220, 219, 217, 216, 215, 214, 213, 212, 210, 209, 208, 207, 206, 
+					204, 203, 202, 200, 199, 198, 196, 195, 194, 192, 191, 190, 188, 187, 186, 184, 183, 
+					181, 180, 178, 177, 176, 174, 173, 171, 170, 168, 167, 165, 164, 162, 161, 159, 158, 
+					156, 155, 153, 151, 150, 148, 147, 145, 144, 142, 141, 139, 137, 136, 134, 133, 131, 
+					130, 128, 126, 125, 123, 122, 120, 119, 117, 115, 114, 112, 111, 109, 108, 106, 105, 
+					103, 101, 100, 98, 97, 95, 94, 92, 91, 89, 88, 86, 85, 83, 82, 80, 79, 78, 76, 75,
+					73, 72, 70, 69, 68, 66, 65, 64, 62, 61, 60, 58, 57, 56, 54, 53, 52, 50, 49, 48, 47, 
+				    46, 44, 43, 42, 41, 40, 39, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 
+				    23, 22, 22, 21, 20, 19, 18, 17, 17, 16, 15, 14, 14, 13, 12, 12, 11, 10, 10, 9, 9, 8, 
+				    7, 7, 6, 6, 6, 5, 5, 4, 4, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 
+				    0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 6, 7, 
+				    7, 8, 9, 9, 10, 10, 11, 12, 12, 13, 14, 14, 15, 16, 17, 17, 18, 19, 20, 21, 22, 22, 
+				    23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 40, 41, 42, 43, 44, 
+				    46, 47, 48, 49, 50, 52, 53, 54, 56, 57, 58, 60, 61, 62, 64, 65, 66, 68, 69, 70, 72, 
+				    73, 75, 76, 78, 79, 80, 82, 83, 85, 86, 88, 89, 91, 92, 94, 95, 97, 98, 100, 101, 
+				    103, 105, 106, 108, 109, 111, 112, 114, 115, 117, 119, 120, 122, 123, 125, 126};
 }
 
 void initPeriods(){
-    periodarray = {secondtonano/256.6, 
-                                secondtonano/277.2,
-                                secondtonano/293.7,
-                                secondtonano/311.1,
-                                secondtonano/329.6,
-                                secondtonano/349.2,
-                                secondtonano/370.0,
-                                secondtonano/392.0,
-                                secondtonano/415.3,
-                                secondtonano/440,
-                                secondtonano/466.2,
-                                secondtonano/493.9};
+    periodarray = (int[]) {secondtonano/261.6,  //C
+		                   secondtonano/277.2,  //C#
+		                   secondtonano/293.7,  //D
+		                   secondtonano/311.1,  //Eb
+		                   secondtonano/329.6,  //E
+		                   secondtonano/349.2,  //F
+		                   secondtonano/370.0,  //F#
+		                   secondtonano/392.0,  //G
+		                   secondtonano/415.3,  //G#
+		                   secondtonano/440,    //A4
+		                   secondtonano/466.2,  //Bb
+		                   secondtonano/493.9}; //B
 }
+
 /******************************************************************************
  Period and Octave
 ******************************************************************************/
 
 void getPeriods(unsigned int notes) {
     int count = 0;
-    for(int i = 0; i < 12; i++) {
+    int i = 0;
+    for(; i < 12; i++) {
         notearray[i] = notes%2;
         if(notes%2 && (count < 3)) {
             periods[count++] = octave_adjust(periodarray[i]);
@@ -229,31 +238,35 @@ unsigned int octave_adjust(unsigned int period) {
 	if (octave > 4){ // shift period up to the higher octave
 		period = period*(2*(octave - 4)); 
 	}
-	else if (new_octave < 4){ // shift period down to the lower octave
+	else if (octave < 4){ // shift period down to the lower octave
 		period = period/(2*(octave)); 
 	}
 	return period;
 }
 
-void octave_read(){
+void octave_read(void){
+	// read in the value of the octave
 	if(PORTBbits.RB3) {
 		octave += canUpdate;
 		canUpdate = 0;
-	} else if(PORTBbits.RB2) {
+	} 
+	else if(PORTBbits.RB2) {
 		octave -= canUpdate;;
 		canUpdate = 0;
-	} else if {
+	} 
+	else {
 		canUpdate = 1;
 	}
 		
 }
 
 void getWave(unsigned int wave) {
+	// determines the wave to use
     switch(wave) {
         case 0x0008 : currWave = square; break;
         case 0x0004 : currWave = sawtooth; break;
-        case 0x0002 : currwave = triangle; break;
-        case 0x0001 : currwave = sine; break;
-        default     : currwave = sine;
+        case 0x0002 : currWave = triangle; break;
+        case 0x0001 : currWave = sine; break;
+        default     : currWave = sine;
     }
 }
