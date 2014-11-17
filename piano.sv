ab626f8bf823929f4484df9616295f4578cddfef
SKrupa <rakuth@gmail.com>
2014-11-16 21:06:57 -0800

    Update piano.sv

--- piano.sv
+++ piano.sv
@@ -9,7 +9,6 @@ module digital_keyboard(input  logic       sck, sdi, clk,
                         output logic [7:0] wave);
 
 	logic [31:0] q;
-	logic sdi,
 	logic [7:0] notes, note1, note2, note3;
     
 	spi_slave_receive_only spi(sck, sdi, q);
@@ -33,7 +32,7 @@ module process_spi( input logic         sck, clk,
                     input logic  [31:0]  q,
                     output logic [7:0] note1, note2, note3);
     logic [6:0] cnt = 6'b00_0000;
-    logic moved = 1'b0; //makes sure iterator doesn't start until initial signal
+    //logic moved = 1'b0; //makes sure iterator doesn't start until initial signal
 	 
 	 always_ff @(negedge sck)
 		if(cnt == 6'd31)
@@ -60,9 +59,9 @@ module attenuation(input  logic       clk,
 				   input  logic [7:0] wave,
 				   output logic [7:0] attenuated);
 
-	logic [31:0] up = 0';
+	logic [31:0] up = '0;
 	logic [3:0] i = 4'd8;
-	logic [23:0] count = 0;
+	logic [23:0] count = '0;
 
 	always_ff@(posedge clk)
 		begin
@@ -70,31 +69,30 @@ module attenuation(input  logic       clk,
 
 		if (up <= 25'h1312D00) // take 0.5 s on the rise
 			begin
-				if (count == 22'h2625A0)
-					begin
-					i <= i - 1'b1;
-					count <= 0;
-					end
-				else 
-					begin
-					count <= count + 1'b1;
-					end
+			if (count == 22'h2625A0)
+				begin
+				i <= i - 1'b1;
+				count <= 0;
+				end
+			else 
+				begin
+				count <= count + 1'b1;
+				end
 			attenuated <= wave >> i;
 			end			
 		else if (up >= 25'h1312D00 & up <= 25'h2625A00) // one it reaches the top (0.5 s)
 			attenuated <= wave;
 		else
 			begin
-			attenuated <= wave << i; // slow down
 			if (count == 24'h969680)
-					begin
-					i <= i + 1'b1;
-					count <= 0;
-					end
-				else 
-					begin
-					count <= count + 1'b1;
-					end
+				begin
+				i <= i + 1'b1;
+				count <= 0;
+				end
+			else 
+				begin
+				count <= count + 1'b1;
+				end
 			attenuated <= wave >> i;
 			end		
 		end
